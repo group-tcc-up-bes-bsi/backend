1b605e8437fc93b10da7b6827f533ee243d55ba6
Gregory de Oliveira <gregoliverdev@gmail.com>
2025-04-24 23:47:45 -0300

    Alter main.ts cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4000',],
+    origin: ['http://localhost:4000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization,X-Requested-With',
